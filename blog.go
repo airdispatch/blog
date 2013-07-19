airdispatch/blog

commit 00c030776ae7da5e61f73cb117d6c0a59549f7b6

Allowed a Single Address to Create Multiple Blogs

blog.go
@@ -26,6 +26,8 @@ type Blog struct {
 	Trackers []string
 	Key *common.ADKey
 
+	BlogId string
+
 	allPosts map[string]Post
 }
 
@@ -65,6 +67,10 @@ func (b *Blog) GetPosts() ([]Post, error) {
 				toFormat.Author = string(dataObject.Payload)
 			} else if *dataObject.TypeName == "airdispat.ch/blog/title" {
 				toFormat.Title = string(dataObject.Payload)
+			} else if *data.Object.TypeName == "airdispat.ch/blog/id" {
+				if (dataObject.Payload) != b.BlogId {
+					continue
+				}
 			}
 		}
 

commit 3b1ee621224878425aa6dd518e7f9c90fbbad3b6

Fixed Errors Introduced by Commit 00c0307

blog.go
@@ -67,14 +67,14 @@ func (b *Blog) GetPosts() ([]Post, error) {
 				toFormat.Author = string(dataObject.Payload)
 			} else if *dataObject.TypeName == "airdispat.ch/blog/title" {
 				toFormat.Title = string(dataObject.Payload)
-			} else if *data.Object.TypeName == "airdispat.ch/blog/id" {
-				if (dataObject.Payload) != b.BlogId {
+			} else if *dataObject.TypeName == "airdispat.ch/blog/id" {
+				if string(dataObject.Payload) != b.BlogId {
 					continue
 				}
 			}
 		}
 
-		toFormat.Date = time.Unix(dataTypes.Timestamp, 0).Format("Jan 2, 2006 at 3:04pm")
+		toFormat.Date = time.Unix(int64(*value.Timestamp), 0).Format("Jan 2, 2006 at 3:04pm")
 
 		if toFormat.Title == "" {
 			continue